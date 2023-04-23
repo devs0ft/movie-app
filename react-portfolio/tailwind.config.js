01502557bd0dd99dd87e1b4a06018d1208d8e53f
devs0ft <devsoft78@gmail.com>
2023-04-23 06:38:55 -0700

    movie-app-starter-files

---
+++ react-portfolio/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		container: {
+			padding: {
+				DEFAULT: "1rem",
+				lg: "0",
+			},
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1170px",
+		},
+		extend: {
+			colors: {
+				primary: "#212353",
+				secondary: "#485068",
+				accent: {
+					primary: "#9C69E2",
+					primary_hover: "#E350A9",
+					tertiary: "#68C9BA",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
